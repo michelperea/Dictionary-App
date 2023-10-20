4116b0aa700b1cc6dc4cba2784566bbe15d396a8
Mike-369 <123855871+michelperea@users.noreply.github.com>
2023-10-19 20:30:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,95 @@
-const wrapper = document.querySelector(".wrapper"),
-searchInput = wrapper.querySelector("input"),
-synonyms = wrapper.querySelector(".synonyms .list"),
-infoText = wrapper.querySelector(".info-text"),
-volumeIcon = wrapper.querySelector(".word i"),
-removeIcon = wrapper.querySelector(".search span");
+const wrapper = document.querySelector(".wrapper");
+const searchInput = wrapper.querySelector("input");
+const synonyms = wrapper.querySelector(".synonyms .list");
+const infoText = wrapper.querySelector(".info-text");
+const volumeIcon = wrapper.querySelector(".word i");
+const removeIcon = wrapper.querySelector(".search span");
 let audio;
 
+function displayWordDetails(word, phonetics, definitions, synonymsList) {
+    // Mostrar la sección de detalles de la palabra.
+    wrapper.classList.add("active");
 
-function data(result, word) {
-    if (result.title){
-        infoText.innerHTML = `Can't find the meaning of  <span>" ${word} "</span>. -Please try to search for another word`;
-    }
-    else {
-        console.log(result);
-        wrapper.classList.add("active");
-        let definitions = result[0].meanings[0].definitions[0],
-        phonetics = `${result[0].meanings[0].partOfSpeech} / ${result[0].phonetics[0].text} / `;
-        
-        document.querySelector('.word p').innerText = result[0].word;
-        document.querySelector('.word span').innerText = phonetics;
-        document.querySelector('.meaning span').innerText = definitions.definition;
-        document.querySelector('.example span').innerText = definitions.example;
-        audio = new Audio('https:' + result[0].phonetics[0].audio);
-        
-        if(definitions.synonyms[0] == undefined){
-            synonyms.parentElement.style.display = 'none';
-        }
-        else {
-            synonyms.parentElement.style.display = 'block';
-            synonyms.innerHTML = "";
-            for( let i =0; i < 5; i++) {
-                let tag = `<span onclick = search('${definitions.synonyms[i]}')>${definitions.synonyms[i]},</span>`;
-                synonyms.insertAdjacentHTML('beforeend', tag);
-            }
+    // Actualizar el contenido de la sección de detalles de la palabra.
+    const wordContainer = document.querySelector('.word');
+    wordContainer.querySelector('p').innerText = word;
+    wordContainer.querySelector('span').innerText = phonetics;
+
+    document.querySelector('.meaning span').innerText = definitions.definition;
+    document.querySelector('.example span').innerText = definitions.example;
+
+    // Crear un objeto de audio para la pronunciación.
+    audio = new Audio('https:' + phonetics.audio);
+
+    // Mostrar hasta 5 sinónimos.
+    if (synonymsList.length === 0) {
+        synonyms.parentElement.style.display = 'none';
+    } else {
+        synonyms.parentElement.style.display = 'block';
+        synonyms.innerHTML = "";
+        for (let i = 0; i < Math.min(5, synonymsList.length); i++) {
+            const tag = `<span onclick="search('${synonymsList[i]}')">${synonymsList[i]},</span>`;
+            synonyms.insertAdjacentHTML('beforeend', tag);
         }
     }
 }
 
-function search (word){
+function displayError(word) {
+    // Mostrar un mensaje de error cuando no se encuentra la palabra.
+    infoText.innerHTML = `No se puede encontrar el significado de <span>"${word}"</span>. Por favor, intenta buscar otra palabra`;
+}
+
+function search(word) {
+    // Establecer la palabra en el campo de búsqueda y realizar una búsqueda.
     searchInput.value = word;
     fetchApi(word);
 }
 
 function fetchApi(word) {
+    // Ocultar la sección de detalles y mostrar un mensaje de búsqueda.
     wrapper.classList.remove('active');
     infoText.style.color = '#000';
-    infoText.innerHTML = `Searching the meaning of<span>"${word}"</span>`;
-    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    // fetching api response and returning it with parsing into js obj
-    //method calling data with passing api response
-    fetch(url).then(res => res.json()).then(result => data(result, word));
+    infoText.innerHTML = `Buscando el significado de <span>"${word}"</span>`;
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((result) => {
+            if (result.title) {
+                // Mostrar un mensaje de error si la palabra no se encuentra.
+                displayError(word);
+            } else {
+                const firstResult = result[0];
+                const { word, phonetics, meanings } = firstResult;
+                const [meaning] = meanings;
+                displayWordDetails(word, phonetics[0], meaning.definitions[0], meaning.synonyms || []);
+            }
+        })
+        .catch(() => {
+            // Manejar errores de red.
+            infoText.innerHTML = 'Se produjo un error al buscar datos. Por favor, inténtalo de nuevo más tarde.';
+        });
 }
 
-
-searchInput.addEventListener('keyup', e => {
-    if(e.key === 'Enter' && e.target.value){
+searchInput.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter' && e.target.value) {
+        // Iniciar una búsqueda cuando se presiona la tecla Enter.
         fetchApi(e.target.value);
     }
 });
 
 volumeIcon.addEventListener('click', () => {
-    audio.play();
+    if (audio) {
+        // Reproducir la pronunciación de la palabra si está disponible.
+        audio.play();
+    }
 });
 
 removeIcon.addEventListener('click', () => {
-    searchInput.value = "";
+    // Limpiar el campo de búsqueda y restablecer la interfaz.
+    searchInput.value = '';
     searchInput.focus();
-    wrapper.classList.remove("active");
+    wrapper.classList.remove('active');
     infoText.style.color = '#9a9a9a';
-    infoText.innerHTML= "Type a word and press enter";
+    infoText.innerHTML = 'Escribe una palabra y presiona Enter';
 });
